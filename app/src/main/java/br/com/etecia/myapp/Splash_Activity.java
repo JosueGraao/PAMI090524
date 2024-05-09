3b3b4708c5e57a50178555148c23000012e6e8a6
JosueGraao <miguelarno@protonmail.com>
2024-05-09 19:54:59 -0300

    + Iterações

--- app/src/main/java/br/com/etecia/myapp/Splash_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Splash_Activity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Splash_Activity extends AppCompatActivity {
+    //Declarando a variável global do tipo componente
+    Button btnAbrirLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,21 @@ public class Splash_Activity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //Apresentar a variável java para a variável xml
+        btnAbrirLogin = findViewById(R.id.btnSplash);
+
+        //Criar uma ação /evento para o componente (Button)
+        btnAbrirLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Instanciando o intent com o contexto e a janela a ser aberta
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                //abrir outra janela com o método startActivity
+                startActivity(intent);
+                //fechando a janela atual
+                finish();
+            }
         });
     }
 }
